72a231a51faee071076528bd127b260c801719d8
Youssef Ouzerbane <youussefouzerbane@gmail.com>
2025-02-04 14:22:07 +0100

    2.1

--- handler/Login.go
+++ handler/Login.go
@@ -5,15 +5,12 @@ import (
 	"net/http"
 )
 
-
-
 func Login(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		email := r.FormValue("email")
 		password := r.FormValue("password")
 
 		fmt.Println("Email:", email, "Password:", password)
-
 
 	}
 }
